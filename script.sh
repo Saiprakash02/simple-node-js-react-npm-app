8d91d7d1cdbb6f2427ccc60d0d427c4797235c7b
Sai Prakash Reddy <108352249+Saiprakash02@users.noreply.github.com>
2024-11-01 19:27:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-sudo docker run --rm -i hadolint/hadolint < Dockerfile > hadolint_output.txt
+docker run --rm -i hadolint/hadolint < Dockerfile > hadolint_output.txt
 cat hadolint_output.txt
